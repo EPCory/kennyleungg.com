a36ccac49baaf94842efe0e5c0920297febca258
Kenny Leung PC <kennyleungg@gmail.com>
2014-04-08 19:35:34 -0400

    Upped the min size of normal and small text

--- js/responsivetext.js
+++ js/responsivetext.js
@@ -27,9 +27,9 @@
       var resizer = function () {
         //Resize text, numbers were obtained manual testing
         $headerText.css('font-size', Math.max(Math.min($root.width() / (25), parseFloat('54px')), parseFloat('32px')));
-        $subheaderText.css('font-size', Math.max(Math.min($root.width() / (35), parseFloat('30px')), parseFloat('20px')));
-        $normalText.css('font-size', Math.max(Math.min($root.width() / (45), parseFloat('26px')), parseFloat('18px')));
-        $smallText.css('font-size', Math.max(Math.min($root.width() / (55), parseFloat('22px')), parseFloat('16px')));
+        $subheaderText.css('font-size', Math.max(Math.min($root.width() / (35), parseFloat('30px')), parseFloat('22px')));
+        $normalText.css('font-size', Math.max(Math.min($root.width() / (45), parseFloat('26px')), parseFloat('20px')));
+        $smallText.css('font-size', Math.max(Math.min($root.width() / (55), parseFloat('22px')), parseFloat('18px')));
         $menuText.css('font-size', Math.max(Math.min($root.width() / (55), parseFloat('24px')), parseFloat('22px')));
 
         var paneText = $root.find(".paneText");
