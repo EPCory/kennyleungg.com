5c2990ddc5ed317ddc641d9a79463bf27a76b68a
Kenny Leung PC <kennyleungg@gmail.com>
2014-04-24 19:40:39 -0400

    Changed responsive text to happen on document ready

--- js/responsivetext.js
+++ js/responsivetext.js
@@ -9,66 +9,57 @@
 * Date: Thu May 05 14:23:00 2011 -0600
 */
 
-(function( $ ){
-  var listen = function(el,on,fn){(el.addEventListener||(on='on'+on)&&el.attachEvent)(on,fn,false)}
 
-  $.fn.fitText = function() {
 
-    return this.each(function(){
+$(document).ready(function () {
+  // Store the object
+  var $root = $(this);
+  var $titleText = $root.find(".titleText");
+  var $headerText = $root.find(".headerText");
+  var $subheaderText = $root.find(".subheaderText");
+  var $normalText = $root.find(".normalText");
+  var $smallText = $root.find(".smallText");
+  var $menuText = $root.find(".menuText");
+  
+  var listen = function(el,on,fn){(el.addEventListener||(on='on'+on)&&el.attachEvent)(on,fn,false)};
+  var resizer = function () {
+    //Resize text, numbers were obtained manual testing
+    $titleText.css('font-size', Math.max(Math.min($root.width() / (20), parseFloat('72px')), parseFloat('42px')));
+    $headerText.css('font-size', Math.max(Math.min($root.width() / (20), parseFloat('54px')), parseFloat('38px')));
+    $subheaderText.css('font-size', Math.max(Math.min($root.width() / (35), parseFloat('36px')), parseFloat('28px')));
+    $menuText.css('font-size', Math.max(Math.min($root.width() / (45), parseFloat('30px')), parseFloat('26px')));
+    $normalText.css('font-size', Math.max(Math.min($root.width() / (55), parseFloat('28px')), parseFloat('22px')));
+    $smallText.css('font-size', Math.max(Math.min($root.width() / (60), parseFloat('24px')), parseFloat('20px')));
 
-      // Store the object
-      var $root = $(this);
-      var $titleText = $root.find(".titleText");
-      var $headerText = $root.find(".headerText");
-      var $subheaderText = $root.find(".subheaderText");
-      var $normalText = $root.find(".normalText");
-      var $smallText = $root.find(".smallText");
-      var $menuText = $root.find(".menuText");
-      
-      var resizer = function () {
-        //Resize text, numbers were obtained manual testing
-        $titleText.css('font-size', Math.max(Math.min($root.width() / (20), parseFloat('72px')), parseFloat('42px')));
-        $headerText.css('font-size', Math.max(Math.min($root.width() / (20), parseFloat('54px')), parseFloat('38px')));
-        $subheaderText.css('font-size', Math.max(Math.min($root.width() / (35), parseFloat('36px')), parseFloat('28px')));
-        $menuText.css('font-size', Math.max(Math.min($root.width() / (45), parseFloat('30px')), parseFloat('26px')));
-        $normalText.css('font-size', Math.max(Math.min($root.width() / (55), parseFloat('28px')), parseFloat('22px')));
-        $smallText.css('font-size', Math.max(Math.min($root.width() / (60), parseFloat('24px')), parseFloat('20px')));
+    var paneText = $root.find(".paneText");
+    var pane = $root.find(".pane");
+    if (paneText && pane) {
+      var paneHeight = pane.outerHeight(true) * 0.95; //5% buffer, don't want text sticking to edge
+      paneText.each(function() {
+        var currentPaneText = $(this);
+        var paneHeader = currentPaneText.find(".paneHeader");
+        var paneBody = currentPaneText.find(".paneBody");
+        var bodyLineItems = paneBody.find("li");
+        var textHeight = currentPaneText.position().top + parseFloat(currentPaneText.css("margin-top")) + paneHeader.outerHeight(true);
+        var bodyHeight = 0;
+        for (var i = 0; i < bodyLineItems.length;i++) {
+          var bodyLineItem = $(bodyLineItems[i]);
+          bodyHeight += bodyLineItem.outerHeight(true);
 
-        var paneText = $root.find(".paneText");
-        var pane = $root.find(".pane");
-        if (paneText && pane) {
-          var paneHeight = pane.outerHeight(true) * 0.95; //5% buffer, don't want text sticking to edge
-          paneText.each(function() {
-            var currentPaneText = $(this);
-            var paneHeader = currentPaneText.find(".paneHeader");
-            var paneBody = currentPaneText.find(".paneBody");
-            var bodyLineItems = paneBody.find("li");
-            var textHeight = currentPaneText.position().top + parseFloat(currentPaneText.css("margin-top")) + paneHeader.outerHeight(true);
-            var bodyHeight = 0;
-            for (var i = 0; i < bodyLineItems.length;i++) {
-              var bodyLineItem = $(bodyLineItems[i]);
-              bodyHeight += bodyLineItem.outerHeight(true);
-
-              if (textHeight + bodyHeight > paneHeight) {
-                bodyLineItem.fadeOut(100);
-              }
-              else {
-                bodyLineItem.fadeIn(100);
-              }
-            }
-          });
+          if (textHeight + bodyHeight > paneHeight) {
+            bodyLineItem.fadeOut(100);
+          }
+          else {
+            bodyLineItem.fadeIn(100);
+          }
         }
-      };
+      });
+    }
+  }
+  // Call once to set.
+  resizer();
 
-      // Call once to set.
-      resizer();
-
-      // Call on resize. Opera debounces their resize by default.
-      listen(window, 'resize', resizer);
-      listen(window, 'load', resizer);
-
-    });
-
-  };
-
-})( jQuery );
+  // Call on resize. Opera debounces their resize by default.
+  listen(window, 'resize', resizer);
+  listen(window, 'load', resizer);
+});
