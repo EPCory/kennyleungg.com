2634a6473c481ae16fc9a1529f21e3a18512274a
Kenny Leung PC <kennyleungg@gmail.com>
2014-04-29 00:44:16 -0400

    Now attempts to grab youtube metadata and displays that instead of loading iFrame

--- js/responsivetext.js
+++ js/responsivetext.js
@@ -12,24 +12,26 @@
 
 
 $(document).ready(function () {
-  // Store the object
   var $root = $(this);
-  var $titleText = $root.find(".titleText");
-  var $headerText = $root.find(".headerText");
-  var $subheaderText = $root.find(".subheaderText");
-  var $normalText = $root.find(".normalText");
-  var $smallText = $root.find(".smallText");
-  var $menuText = $root.find(".menuText");
   
   var listen = function(el,on,fn){(el.addEventListener||(on='on'+on)&&el.attachEvent)(on,fn,false)};
   var resizer = function () {
+    
+    var titleText = $root.find(".titleText");
+    var headerText = $root.find(".headerText");
+    var subheaderText = $root.find(".subheaderText");
+    var normalText = $root.find(".normalText");
+    var smallText = $root.find(".smallText");
+    var menuText = $root.find(".menuText");
+
+
     //Resize text, numbers were obtained manual testing
-    $titleText.css('font-size', Math.max(Math.min($root.width() / (20), parseFloat('72px')), parseFloat('42px')));
-    $headerText.css('font-size', Math.max(Math.min($root.width() / (20), parseFloat('54px')), parseFloat('38px')));
-    $subheaderText.css('font-size', Math.max(Math.min($root.width() / (35), parseFloat('36px')), parseFloat('28px')));
-    $menuText.css('font-size', Math.max(Math.min($root.width() / (45), parseFloat('30px')), parseFloat('26px')));
-    $normalText.css('font-size', Math.max(Math.min($root.width() / (55), parseFloat('28px')), parseFloat('22px')));
-    $smallText.css('font-size', Math.max(Math.min($root.width() / (60), parseFloat('24px')), parseFloat('20px')));
+    titleText.css('font-size', Math.max(Math.min($root.width() / (20), parseFloat('72px')), parseFloat('42px')));
+    headerText.css('font-size', Math.max(Math.min($root.width() / (20), parseFloat('54px')), parseFloat('38px')));
+    subheaderText.css('font-size', Math.max(Math.min($root.width() / (35), parseFloat('36px')), parseFloat('28px')));
+    menuText.css('font-size', Math.max(Math.min($root.width() / (45), parseFloat('30px')), parseFloat('26px')));
+    normalText.css('font-size', Math.max(Math.min($root.width() / (55), parseFloat('28px')), parseFloat('22px')));
+    smallText.css('font-size', Math.max(Math.min($root.width() / (60), parseFloat('24px')), parseFloat('20px')));
 
     var paneText = $root.find(".paneText");
     var pane = $root.find(".pane");
--- js/youtubehandler.js
+++ js/youtubehandler.js
@@ -2,16 +2,42 @@ $(document).ready(function () {
 	var videoWrappers = $(".youtubeWrapper");
 	for (var videoCount = 0; videoCount < videoWrappers.length; videoCount++){
 		var videoWrap = $(videoWrappers[videoCount]);
-		var videoID = videoWrap.attr("videoID");
-		var coverPhotoLink = "http://img.youtube.com/vi/" + videoID + "/maxresdefault.jpg";
-		var thumbnailLink = "http://img.youtube.com/vi/" + videoID + "/hqdefault.jpg";
+		var videoID = videoWrap.attr("id");
+		
+		var req = $.get( "https://gdata.youtube.com/feeds/api/videos/"+videoID+"?v=2", function( data ) 
+		{
+			var coverPhotoLink = "http://img.youtube.com/vi/" + videoID + "/maxresdefault.jpg";
+			videoWrap.css("background-image","url(" + coverPhotoLink + ")");
+			videoWrap.append("<div class='youtubeBackgroundGradient'></div>");
 
-		videoWrap.css("background-image","url(" + coverPhotoLink + ")");
-		videoWrap.append("<div class='youtubeBackgroundGradient'></div>");
-		videoWrap.append("<iframe src='http://www.youtube.com/embed/" +videoID+"' frameborder='0' allowfullscreen></iframe>");
-		// videoWrap.append("<div class='youtubeThumbnail' style=background-image:"+"url(" + coverPhotoLink + ")></div>");
+			var textWrap = $(document.createElement("div"));
+			textWrap.addClass("youtubeText").addClass("light");
+			textWrap.append("<div class='youtubeTitle normalText'>"+$(data).find("entry").find("title")[0].textContent+"</div>");
+			textWrap.append("<div class='youtubeAuthor smallText'> "+$(data).find("entry").find("author").find("name")[0].textContent+" </div>");
+			videoWrap.append(textWrap);
+
+			var centerWrap = $(document.createElement("div"));
+			centerWrap.addClass("centeredWrapper");
+			centerWrap.append("<img class='youtubePlay' src='images/YT.png' onclick=\"getYoutubeIFrame('" + videoID + "')\" onmouseover=\"this.src='images/YT-Hover.png'\" onmouseout=\"this.src='images/YT.png'\"></img>");
+			videoWrap.append(centerWrap);
+		}).fail(function(){
+			var centerWrap = $(document.createElement("div"));
+			centerWrap.addClass("centeredWrapper");
+			centerWrap.append("<div class='youtubeFailMessage normalText light'>Something went wrong! Could not load video</div>");
+
+			videoWrap.append(centerWrap);
+		});
+		
+		
 
 	}
 });
+
+function getYoutubeIFrame(youtubeID) {
+	var videoWrap = $("#" + youtubeID);
+	videoWrap.append("<iframe src='http://www.youtube.com/embed/" +youtubeID+"?autoplay=1' frameborder='0' allowfullscreen></iframe>");
+	videoWrap.children(".youtubeText").remove();
+	videoWrap.children(".youtubePlayWrapper").remove();
+}
 
 // videoWrap.append("<iframe src='http://www.youtube.com/embed/" +videoID+"' frameborder='0' allowfullscreen></iframe>");
