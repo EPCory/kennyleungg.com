494609991293aab2b275497ad95ea3bccb14bc8a
Kenny Leung PC <kennyleungg@gmail.com>
2014-05-08 18:50:19 -0400

    Puting menubar into menu.php

--- blog.php
+++ blog.php
@@ -9,15 +9,9 @@
 	<link rel='stylesheet' type='text/css' href='css/open-sans.css'>
 	<script type="text/javascript" src="js/responsivetext.js"></script>
 	<script type="text/javascript" src="js/imageloader.js"></script>
-	<script type="text/javascript" src="js/menubar.js"></script>
 </head>
 <body class="bg">
-<div id="menuBar">
-	<a href="" class="light menuText"> Home </a>
-	<a href="about.html" class="light menuText"> About Me </a>
-	<a href="blog.html" class="light menuText"> Blog </a>
-	<a href="contact.html" class="light menuText"> Contact </a>
-</div>
+<?php include('html/menu.php'); ?>
 <?php 
 
 	//database server 
