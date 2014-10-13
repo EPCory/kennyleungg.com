2950f1dc8ff6bef2b95a41410f0f64b073953e82
Kenny Leung <kennyleungg@gmail.com>
2014-10-12 21:40:00 -0700

    Dev

--- blog.php
+++ blog.php
@@ -6,16 +6,22 @@
 	<link rel="icon" type="image/png" href="images/icon.png">
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/main.css"/>
+	<link rel="stylesheet" type="text/css" href="css/blog.css"/>
 	<link rel='stylesheet' type='text/css' href='css/open-sans.css'>
 	<script type="text/javascript" src="js/responsivetext.js"></script>
 	<script type="text/javascript" src="js/imageloader.js"></script>
 </head>
-<body class="bg">
+<body id="bg">
 <?php include('html/menu.php'); ?>
+<div id="coverWrapper">
+	<div id="coverGradient"></div>
+	<div id="coverPhoto" style="background-image:url('images/Blog-Header.jpg')"></div>
+	<div id="coverTitle" class="light">
+		<div class="titleText">The Aimless Blog</div>
+		<div class="smallText">Kenny's arbritrary thoughts and opinions on just about everything</div>
+	</div>
+</div>
 <?php 
-
-	//database server 
-	define('db_server', 'localhost'); 
 
 	//database
 	$db_dbname = 'Blog'; 
@@ -27,28 +33,37 @@
 	function outputQueryResults() { 
 	   
 	  //run a select query 
-	  $select_query = 'SELECT * FROM Posts'; 
-	  $result = mysql_query($select_query); 
-	   
-	  //output data in a table 
-	  echo "<table>\n"; 
-	  while ($row = mysql_fetch_row($result)){     
-	      echo "<tr>\n"; 
-	      foreach ($row as $val) { 
-	          echo "<td>$val</td>\n"; 
-	      } 
-	      echo "</tr>\n"; 
-	  } 
-	  echo '</table>'; 
+	  $select_query = 'SELECT * FROM Posts ORDER BY post_timestamp DESC'; 
+	  $result = mysql_query($select_query);
+	while ($row = mysql_fetch_row($result)) {
+		echo "<div class='blogEntryWrapper'>";  
+		echo "<div class='blogEntryGradient'></div>";
+		echo "<div class='blogEntryPhoto' style=\"background-image:url('$row[3]')\"></div>";
+		echo "<div class='headerText light blogEntryTitle'>$row[1]</div>";
+		$post_date = date_create($row[2]);
+		echo "<div class='smallText light blogEntryTime'>Posted: ";
+		echo date_format($post_date, 'Y-m-d');
+		echo "</div>";
+		echo "</div>";	
+}//output data in a table 
+	  //echo "<table>\n"; 
+	  //while ($row = mysql_fetch_row($result)){     
+	      //echo "<tr>\n"; 
+	      //foreach ($row as $val) { 
+	          //echo "<td>$val</td>\n"; 
+	      //} 
+	      //echo "</tr>\n"; 
+	  //} 
+	  //echo '</table>'; 
 	} 
+	
 
-	//connect to the database server 
-	$db = mysql_connect(db_server); 
+	//connect to the database server (plaintext creds, but read only account)
+	$db = mysql_connect('localhost','public_user','public1'); 
 	if (!$db) { 
 	   die('Could Not Connect: ' . mysql_error()); 
-	} else { 
-	  echo "Connected Successfully...\n"; 
-	} 
+	}
+
 	//select database name 
 	mysql_select_db($db_dbname); 
 
