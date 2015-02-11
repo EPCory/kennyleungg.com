1eaca657cb92da3186789076b4e7ccf019609e39
Kenny <Kenny@v1020-wn-5-59.campus-dynamic.uwaterloo.ca>
2015-02-11 15:34:50 -0500

    Removed old files

--- html/menu.php
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript" src="js/menubar.js"></script>
-<div id="menuBar">
-	<a href="" class="light menuText"> Home </a>
-	<a href="about.html" class="light menuText"> About Me </a>
-	<a href="blog.html" class="light menuText"> Blog </a>
-	<a href="contact.html" class="light menuText"> Contact </a>
-</div>
